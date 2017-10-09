ab002374eeddb44184033415abdd9cbc89deceef
Dzung Nguyen <dzungng89@gmail.com>
2017-10-09 15:44:25 -0400

    generalization doesn't work well

--- visiprog/evaluate.py
+++ visiprog/evaluate.py
@@ -2,37 +2,45 @@ from sklearn.neighbors import NearestNeighbors
 import numpy as np 
 from .data import read_material_label
 from sklearn.model_selection import train_test_split
+import logging
 
 
-def leave_one_sample_out(X, label):
+logger = logging.getLogger(__name__)
+
+
+def leave_one_sample_out(X_train, Y_train, X_test = None, Y_test=None):
 	'''
 	given path to a feature for each sample
 	calculate the leave one sample out accuracy
 	'''
 
 	# one nearest neighbor
-	nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree').fit(X)
-	distances, indices = nbrs.kneighbors(X)
 
-	prediction = label[indices[:,1]]
+	nbrs = NearestNeighbors(n_neighbors=2, algorithm='auto').fit(X_train)
 
-	accuracy = np.mean(prediction == label)
+	if X_test is None and Y_test is None:
+		logger.info("Nearest neighbor on same data")
+		distances, indices = nbrs.kneighbors(X_train)
+
+		# since test and train are same, first will return duplicate
+		prediction = Y_train[indices[:,1]]
+		accuracy = np.mean(prediction == Y_train)
+
+	elif not X_test is None and not Y_test is None:
+		logger.info("Nearest neighbor on separate test data")
+		distances, indices = nbrs.kneighbors(X_test)
+
+		# print(distances)
+		# print(indices)
+
+		prediction = Y_train[indices[:,0]]
+		accuracy = np.mean(prediction == Y_test)
+	else:
+		raise ValueError
+
 
 	results = {}
 	results['accuracy'] = accuracy
 	results['prediction'] = prediction
 
 	return results
-
-
-# def split_material_validate(X, Y, ratio=0.5):
-
-# 	indices = np.arange(X.shape[0])
-
-# 	label_material = read_material_label()
-
-# 	X_train, X_test, Y_train, Y_test = \
-# 		train_test_split(X, Y, indices, test_size=ratio, random_state=42, stratify=Y)
-
-
-# 	return X_train, X_test, Y_train, Y_test
